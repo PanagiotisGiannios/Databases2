7727130a7cab58edf8f4d5da6f3edbffed69ef82
PanagiotisGiannios <panagiotisgiannios02@gmail.com>
2023-12-13 19:58:49 +0200

    Added data for professors and projects
    Created data for professors and projects. This data I believe is enough for our needs at the moment.

---
+++ Schema Folder/professors.sql
@@ -0,0 +1,22 @@
+INSERT INTO PROFESSOR (ProfId, ManagerId, Profession)
+VALUES
+    (19900001, NULL, 'Computer Hardware and Architecture'),
+    (19920002, 19900001, 'Signals and Communications'),
+    (19880003, 19900001, 'Applications and Foundations of Computer Science'),
+    (19950004, 19900001, 'Energy'),
+    (19910005, 19900001, 'Software and Information System Engineering'),
+    (19930006, 19900001, 'Software and Information System Engineering'),
+    (19890007, 19900001, 'Energy'),
+    (19940008, 19900001, 'Applications and Foundations of Computer Science'),
+    (19920009, 19900001, 'Computer Hardware and Architecture'),
+    (19960010, 19900001, 'Energy'),
+    (19900011, 19900001, 'Software and Information System Engineering'),
+    (19930012, 19900001, 'Applications and Foundations of Computer Science'),
+    (19950013, 19900001, 'Signals and Communications'),
+    (19920014, 19900001, 'Signals and Communications'),
+    (19880015, 19900001, 'Energy'),
+    (19940016, 19900001, 'Applications and Foundations of Computer Science'),
+    (19960017, 19900001, 'Software and Information System Engineering'),
+    (19890018, 19900001, 'Signals and Communications'),
+    (19910019, 19900001, 'Applications and Foundations of Computer Science'),
+    (19970020, 19900001, 'Software and Information System Engineering');
---
+++ Schema Folder/projects.sql
@@ -0,0 +1,17 @@
+INSERT INTO PROJECT (ProfessorId, Name, Field, Type, Information)
+VALUES
+    (19900001, 'Project 1', 'Comp. Architecture & Hardware', 'Research', 'Description 1'),
+    (19920002, 'Project 2', 'Signals & Communications', 'Development', 'Description 2'),
+    (19880003, 'Project 3', 'Apps & Foundations of C.S.', 'Education', 'Description 3'),
+    (19950004, 'Project 4', 'Energy', 'Research', 'Description 4'),
+    (19910005, 'Project 5', 'Software & Info. Sys. Eng.', 'Development', 'Description 5'),
+    (19930006, 'Project 6', 'Software & Info. Sys. Eng.', 'Development', 'Description 6'),
+    (19890007, 'Project 7', 'Energy', 'Research', 'Description 7'),
+    (19940008, 'Project 8', 'Apps & Foundations of C.S.', 'Education', 'Description 8'),
+    (19920009, 'Project 9', 'Comp. Architecture & Hardware', 'Development', 'Description 9'),
+    (19960010, 'Project 10', 'Energy', 'Research', 'Description 10'),
+    (19900011, 'Project 11', 'Software & Info. Sys. Eng.', 'Development', 'Description 11'),
+    (19930012, 'Project 12', 'Apps & Foundations of C.S.', 'Education', 'Description 12'),
+    (19950013, 'Project 13', 'Signals & Communications', 'Development', 'Description 13'),
+    (19920014, 'Project 14', 'Software & Info. Sys. Eng.', 'Development', 'Description 14'),
+    (19880015, 'Project 15', 'Energy', 'Research', 'Description 15');
