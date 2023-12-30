5d8ca8a7687e1581d29174be0ba9ec4559f02f1f
Fragk1 <fragkmakris@gmail.com>
2023-12-30 14:39:11 +0200

    Fully complete the Add functionality
    Everything in Add Page Professor exept Project section

--- universitymanager/src/main/java/code/AddPage.java
+++ universitymanager/src/main/java/code/AddPage.java
@@ -16,6 +16,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -32,22 +33,13 @@ public class AddPage extends Page {
     private String type;
     private String background;
 
-    // Employee Features
-    private String ssn;
-    private String fname;
-    private String lname;
-    private String sex;
-    private String phone;
-    private String email;
-    private String jobStartingDate;
-    private String birthday;
-    private String address;
-    private String salary;
+    // Components
+    List<TextField> textComponents = null;
+    List<VBox> radioComponents = null;
+    List<DatePicker> dateComponents = null;
 
-    // Professor feature
-    private String profession;
+    private List<String> entry;
 
-    private String[] entry = null;
 
     public AddPage(String type) {
         this.type = type;
@@ -80,6 +72,7 @@ public class AddPage extends Page {
         createScene();
     }
 
+    // Creates the Add Pages along the type.
     private void createAddPage() {
          switch (type) {
             case "professor":
@@ -101,6 +94,7 @@ public class AddPage extends Page {
          }
     }
 
+    // Creates a Professor Add Page.
     private void createAddPageProfessor() {
         VBox base = new VBox();
         HBox titleBox = new HBox();
@@ -113,21 +107,30 @@ public class AddPage extends Page {
         titleBox.getChildren().add(titleLabel);
         titleBox.setAlignment(Pos.TOP_CENTER);
 
+        // Setup the components List
+        textComponents = new ArrayList<>();
+        radioComponents = new ArrayList<>();
+        dateComponents = new ArrayList<>();
+
         // Set up the features
         TextField fname = new TextField();
         fname.setPromptText("First Name");
+        textComponents.add(fname);
 
         TextField lname = new TextField();
         lname.setPromptText("Last Name");
+        textComponents.add(lname);
 
-        TextField ssn = new TextField();
+        TextField ssn = createNumericTextField();
         ssn.setPromptText("Social Security Number");
+        textComponents.add(ssn);
         
         TextField phone = createNumericTextField();
         phone.setPromptText("Phone Number");
+        textComponents.add(phone);
 
         // Sex Radio Button
-        Label gender = new Label("Gender");
+        Label gender = new Label("Sex");
         gender.setFont(Font.font(15));
         gender.setStyle("-fx-font-weight: bold;");
         RadioButton maleRadioButton = new RadioButton("Male");
@@ -139,21 +142,27 @@ public class AddPage extends Page {
 
         VBox sex = new VBox(gender, maleRadioButton, femaleRadioButton);
         sex.setPadding(new Insets(5));
+        radioComponents.add(sex);
 
         TextField address = new TextField();
         address.setPromptText("Address");
+        textComponents.add(address);
 
         TextField salary = createNumericTextField();
         salary.setPromptText("Salary");
+        textComponents.add(salary);
 
         TextField email = new TextField();
         email.setPromptText("Email Address");
+        textComponents.add(email);
         
         DatePicker birthday = new DatePicker();
         birthday.setPromptText("Birthday");
+        dateComponents.add(birthday);
 
         DatePicker jobStartingDate = new DatePicker();
         jobStartingDate.setPromptText("Job Starting Date");
+        dateComponents.add(jobStartingDate);
 
         // Profession Radio Buttons setup
         Label profLabel = new Label("Profession");
@@ -174,8 +183,9 @@ public class AddPage extends Page {
 
         VBox profession = new VBox(profLabel, radio1, radio2, radio3, radio4, radio5);
         profession.setPadding(new Insets(5));
+        radioComponents.add(profession);
 
-        VBox featureSide = new VBox(fname, lname, ssn, phone, sex, address, salary, email, birthday, jobStartingDate, profession);
+        VBox featureSide = new VBox(textComponents.get(0), textComponents.get(1), textComponents.get(2), textComponents.get(3), radioComponents.get(0), textComponents.get(4), textComponents.get(5), textComponents.get(6), dateComponents.get(0), dateComponents.get(1), radioComponents.get(1));
         featureSide.setAlignment(Pos.CENTER_LEFT);
         featureSide.setSpacing(8);
         featureSide.setPadding(new Insets(10, 450, 0, 0));
@@ -189,35 +199,39 @@ public class AddPage extends Page {
         bottomBox.setAlignment(Pos.CENTER);
 
         addButton.setOnAction(event -> {
-            String[] newEntry = new String[10];
-            this.ssn = newEntry[0] = ssn.getText();
+            entry = new ArrayList<>();
+            
+            entry.add(ssn.getText());
+            entry.add(fname.getText());
+            entry.add(lname.getText());
 
-            this.fname = newEntry[1] = fname.getText();
+            try {
+                RadioButton selectedGender = (RadioButton) toggleGroup.getSelectedToggle();
+                entry.add(selectedGender.getText());
+            }
+            catch (NullPointerException e) {
+                entry.add(null);
+            }
 
-            this.lname = newEntry[2] = lname.getText();
-
-            RadioButton selectedGender = (RadioButton) toggleGroup.getSelectedToggle();
-            this.sex = newEntry[3] = selectedGender.getText();
-
-            this.phone = newEntry[4] = phone.getText();
-
-            this.email = newEntry[5] = email.getText();
+            entry.add(phone.getText());
+            entry.add(email.getText());
 
             LocalDate jobStartingDateValue  = birthday.getValue();
-            this.jobStartingDate = newEntry[6] = jobStartingDateValue  == null ? "" : jobStartingDateValue .toString();
+            entry.add(jobStartingDateValue  == null ? "" : jobStartingDateValue .toString());
 
             LocalDate birthdayDate = birthday.getValue();
-            this.birthday = newEntry[7] = birthdayDate == null ? "" : birthdayDate.toString();
+            entry.add(birthdayDate == null ? "" : birthdayDate.toString());
 
-            this.address = newEntry[8] = address.getText();
+            entry.add(address.getText());
+            entry.add(salary.getText());
 
-            this.salary = newEntry[9] = salary.getText();
-
-            RadioButton selectedProfession = (RadioButton) professionGroup.getSelectedToggle();
-            this.profession = selectedProfession.getText();
-
-            this.entry = newEntry;
-            handleButtonPress(addButton);
+            try {
+                RadioButton selectedProfession = (RadioButton) professionGroup.getSelectedToggle();
+                entry.add(selectedProfession.getText());
+            }
+            catch (NullPointerException e) {
+                entry.add(null);
+            }
         });
 
 
@@ -234,6 +248,7 @@ public class AddPage extends Page {
         root.getChildren().addAll(base);
     }
 
+    // Make the a button have a Transition.
     private void setButtonProperties(Button button){
         ScaleTransition hoverTransition = new ScaleTransition(Duration.millis(100),button);
         ScaleTransition clickTransition = new ScaleTransition(Duration.millis(50),button);
@@ -278,58 +293,114 @@ public class AddPage extends Page {
         });
 
         button.setFocusTraversable(false);
-
     }
 
+    // 
     private void handleButtonPress(Button button) {
+        if (type == "professor" || type == "auxilery") {
+            if (check()) {
+                addEmployee();
+                if (type == "professor") {
+                    addProfessor();
+                }
+                else {
+                    // TODO:  addAuxilery();
+                }
+                clearFields();                
+            }
+            else {
+                // TODO: fill this
+            }
+        }
+        // TODO: if for the rest
+        else if (true) {
+
+        }       
+    }
+
+    private boolean check() {
+        if (!checkForMissingValues()) {
+            showErrorAlert("Missing Values", "Some values are missing", "Please fill all the cells");
+            return false;
+        }
+        else if (!checkForDuplicate()) {
+            showErrorAlert("Duplicate SSN", "SSN already exists", "An employee with the same SSN already exists.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkForMissingValues() {
+        for (String e : entry) {
+            if (e == null || e == "") {
+                System.out.println(e);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkForDuplicate() {
+        // Check for duplicates
+        String checkDuplicateQuery = "SELECT COUNT(*) FROM EMPLOYEE WHERE SSN = ?";
+        try (PreparedStatement checkDuplicateStatement = connection.prepareStatement(checkDuplicateQuery)) {
+            checkDuplicateStatement.setString(1, entry.get(0));
+
+            try (ResultSet resultSet = checkDuplicateStatement.executeQuery()) {
+                resultSet.next();
+                int count = resultSet.getInt(1);
+
+                // SSN already exists
+                if (count > 0) {
+                    return false;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+
+    private void addEmployee() {
         // Insert in the Employee Table
-        boolean error = false;
         String insertEmployeeQuery = "INSERT INTO EMPLOYEE (SSN, FirstName, LastName, Sex, Phone, Email, JobStartingDate, Birthday, Address, Salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(insertEmployeeQuery)) {
-            preparedStatement.setString(1, this.ssn);
-            preparedStatement.setString(2, this.fname);
-            preparedStatement.setString(3, this.lname);
-            preparedStatement.setString(4, this.sex);
-            preparedStatement.setString(5, this.phone);
-            preparedStatement.setString(6, this.email); 
-            preparedStatement.setString(7, this.jobStartingDate);
-            preparedStatement.setString(8, this.birthday);
-            preparedStatement.setString(9, this.address);
-            preparedStatement.setString(10, this.salary);
+            for (int i = 0; i < 10; i++) {
+                preparedStatement.setString(i+1, entry.get(i));
+            }
 
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0) {
-                error = false;                
-            } else {
-                error = true;
-                showErrorAlert("Problem", "Failed to insert professor.", "An error occurred. Please check your input.");
+            if (preparedStatement.executeUpdate() <= 0) {
+                showErrorAlert("Problem", "Failed to insert employee.", "An error occurred. Please check your input.");
             }
         }
         catch (SQLException e) {
-        e.printStackTrace();
+            e.printStackTrace();
         }
+    }
 
+    private void addProfessor() {
         // Find who is the manager
-        String whoIsManager = "SELECT DISTINCT * FROM PROFESSOR WHERE ManagerId IS NULL";
+        String whoIsManager = "SELECT ProfId FROM PROFESSOR WHERE ManagerId IS NULL";
         String manager = "";
         try (ResultSet result = Page.connection.createStatement().executeQuery(whoIsManager)){
-            manager = result.getString("ManagerId");
-        } catch (Exception e) {
+            result.next();
+            manager = result.getString("ProfId");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        // Insert to Professors table
+        // Insert to Professor table
         String insertProfessorQuery = "INSERT INTO PROFESSOR (ProfId, ManagerId, Profession) VALUES (?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(insertProfessorQuery)) {
-            preparedStatement.setString(1, this.ssn);
+            preparedStatement.setString(1, entry.get(0));
             preparedStatement.setString(2, manager);
-            preparedStatement.setString(3, this.profession);
+            preparedStatement.setString(3, entry.get(entry.size()-1));
 
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0 && error == false) {
+            if (preparedStatement.executeUpdate() > 0) {
                 showSuccessAlert("Success", "Operation Successful", "The operation was completed successfully.");
             }
-            else if (error == false) {
+            else {
                 showErrorAlert("Problem", "Failed to insert professor.", "An error occurred. Please check your input.");
             }
         } catch (SQLException ex) {
@@ -372,6 +443,27 @@ public class AddPage extends Page {
         textField.setTextFormatter(textFormatter);
 
         return textField;
+    }
+
+    // Clear all the fields.
+    private void clearFields() {
+        for (TextField text : textComponents) {
+            text.clear();
+        }
+        
+        for (DatePicker date : dateComponents) {
+            date.setValue(null);
+        }
+
+        for (VBox vbox : radioComponents) {
+            for (Node childNode : vbox.getChildren()) {
+                if (childNode instanceof RadioButton) {
+                    RadioButton radioButton = (RadioButton) childNode;
+                    radioButton.setSelected(false);
+                }
+            }
+        }
+        
     }
 
     public static void main(String[] args) {
