cb267fb29b0609642def391ceb6237f3b10905bd
Fragk1 <fragkmakris@gmail.com>
2024-01-11 16:29:36 +0200

    Update auxiliary_staff.sql

--- Schema Folder/auxiliary_staff.sql
+++ Schema Folder/auxiliary_staff.sql
@@ -1,12 +1,12 @@
 INSERT INTO auxiliary_staff (EmployeeID, profession)
 VALUES
     (19880024, 'Secretariat'),
-    (19900027, 'IT Support Specialist'),
-    (19920023, 'Lab Technician'),
-    (19920030, 'Cleaning Staff'),
-    (19930021, 'IT Support Specialist'),
+    (19900027, 'Security Guard'),
+    (19920023, 'Maintenance'),
+    (19920030, 'Cleaning Personnel'),
+    (19930021, 'Security Guard'),
     (19930028, 'Secretariat'),
     (19940025, 'Secretariat'),
-    (19950022, 'Cleaning Staff'),
-    (19950029, 'Student Advisor'),
-    (19960026, 'Facilities Manager');
+    (19950022, 'Cleaning Personnel'),
+    (19950029, 'Accountant'),
+    (19960026, 'Accountant');
